e193c570188afab0ea313144d97b0e17e48ff7a9
santhoshkumarchellamuthu <santhoshkumarcsk45@gmail.com>
2023-11-05 19:38:49 +0530

    second commit

--- main.js
+++ main.js
@@ -66,17 +66,104 @@ document.addEventListener("DOMContentLoaded", function () {
       const rowIndex = [...Data.parentElement.parentElement.children].indexOf(
         Data.parentElement
       );
-      console.log(rowIndex);
-      console.log(colIndex);
+
       Data.rowSpan = 1;
       Data.colSpan = parseInt(Time);
+      Data.setAttribute("draggable", "true");
       Data.innerHTML = Task;
       Data.classList.add("color");
+      let block = null;
 
       for (let i = colIndex + 1; i < colIndex + parseInt(Time); i++) {
         if (Data.parentElement.children[i]) {
           Data.parentElement.children[i].style.display = "none";
+          block = Data.parentElement.children[i];
         }
+
+        const tds = document.querySelectorAll(".tablebody td");
+        let dragging = null;
+        let previous = null;
+        const clearCellContent = (td) => {
+          td.innerHTML = "";
+          td.classList.remove("color");
+          td.colSpan = 1;
+          td.style.display = "table-cell";
+
+          const row = td.parentElement.children;
+          const cellIndex = Array.from(row).indexOf(td);
+
+          for (let i = cellIndex + 1; i < row.length; i++) {
+            if (row[i].style.display === "none") {
+              row[i].style.display = "table-cell";
+              if (row[i].colSpan > 1) {
+                for (let j = 1; j < row[i].colSpan; j++) {
+                  const cellToRestore = row[i + j];
+
+                  cellToRestore.innerHTML = "";
+                  cellToRestore.classList.remove("color");
+                  cellToRestore.style.display = "table-cell";
+                }
+              }
+            } else {
+              break;
+            }
+          }
+        };
+        tds.forEach((td) => {
+          td.addEventListener("dblclick", function (event) {
+            clearCellContent(td);
+          });
+          let hiddenCells = [];
+          td.addEventListener("dragstart", function (event) {
+            if (td.innerHTML !== "") {
+              previous = td;
+              event.dataTransfer.setData("text/plain", previous.innerHTML);
+              previous.innerHTML = "";
+              previous.classList.remove("color");
+              previous.colSpan = 1;
+
+              const row = td.parentElement.children;
+              const cellIndex = Array.from(row).indexOf(td);
+              for (let i = cellIndex + 1; i < row.length; i++) {
+                if (row[i].style.display === "none") {
+                  row[i].style.display = "table-cell";
+                  if (row[i].colSpan > 1) {
+                    for (let j = 1; j < row[i].colSpan; j++) {
+                      const cellToRestore = row[i + j];
+                      cellToRestore.innerHTML = "";
+                      cellToRestore.classList.remove("color");
+                      cellToRestore.style.display = "table-cell";
+                    }
+                  }
+                } else {
+                  break;
+                }
+              }
+            }
+          });
+
+          td.addEventListener("dragover", function (event) {
+            event.preventDefault();
+          });
+
+          td.addEventListener("drop", (event) => {
+            event.preventDefault();
+            const data = event.dataTransfer.getData("text/plain");
+            console.log(data);
+            if (td.innerHTML === "") {
+              td.innerHTML = data;
+              td.colSpan = parseInt(data.split(",")[1]);
+              td.classList.add("color");
+
+              hiddenCells.forEach((hiddenCell) => {
+                hiddenCell.style.display = "table-cell";
+                hiddenCell.innerHTML = "";
+                hiddenCell.classList.remove("color");
+              });
+              hiddenCells = [];
+            }
+          });
+        });
       }
     });
   });
